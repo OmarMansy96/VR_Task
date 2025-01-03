f3b5ec6cd94fb78cda322ac38530b731fe6ddf8a
Omar Mansy <omarmansy96@gmail.com>
2025-01-03 11:41:57 +0200

    Finished

--- Assets/Scripts/TargetsSystem.cs
+++ Assets/Scripts/TargetsSystem.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class TargetsSystem : MonoBehaviour
 {
-    public GameObject[] targetPrefabs; 
-    public Transform[] spawnPoints;  
-    public float spawnInterval = 2f; 
+    public GameObject[] targetPrefabs;
+    public Transform[] spawnPoints;
+    public float spawnInterval = 2f;
     public float hideTarget;
     GameModes gameMode;
 
     void Start()
     {
-        gameMode=FindObjectOfType<GameModes>();
+        gameMode = FindObjectOfType<GameModes>();
         StartCoroutine(SpawnTargets());
     }
 
@@ -20,24 +20,24 @@ public class TargetsSystem : MonoBehaviour
     {
         while (true)
         {
-            if (gameMode.isGameRunning)
-            {
-                SpawnTarget();
-                yield return new WaitForSeconds(spawnInterval);
-            }
-           
+
+            SpawnTarget();
+            yield return new WaitForSeconds(spawnInterval);
+
+
         }
-       
+
     }
 
     void SpawnTarget()
     {
-        //if (GameModes.isGameRunning==false) return;
-    
+
         int randomIndex = Random.Range(0, targetPrefabs.Length);
         int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        print("before");
+        if (gameMode.isGameRunning == false) return;
 
-
+        print("after");
         GameObject newTarget = Instantiate(targetPrefabs[randomIndex], spawnPoints[spawnPointIndex].position, Quaternion.identity);
         Destroy(newTarget, hideTarget);
         //Destroy(Instantiate(targetPrefabs[randomIndex], spawnPoints[spawnPointIndex].position, Quaternion.identity), hideTarget);
